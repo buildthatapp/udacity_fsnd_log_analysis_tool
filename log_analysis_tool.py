c7824caed4ba294ea264766fc68fce12040429f0
Louis Magdaleno <buildthatapp@gmail.com>
2018-09-09 21:27:27 -0700

    Update to log_analysis_tool.py
    Added function day_with_high_errors()
    Checked for PEP8 compliance on pep8online.com
    Checking in code that is PEP8 compliant.

--- log_analysis_tool.py
+++ log_analysis_tool.py
@@ -4,8 +4,9 @@ import psycopg2
 
 DBNAME = "news"
 
+
 def three_most_popular_articles():
-    """Queries and returns top three most viewed articles in the news database."""
+    """Queries and displays the top three most viewed articles."""
     conn = psycopg2.connect(database=DBNAME)
 
     cur = conn.cursor()
@@ -19,7 +20,7 @@ def three_most_popular_articles():
     cur.close()
 
     conn.close()
-    
+
     print()
     print('Three most popular articles of all time')
     print('=======================================')
@@ -30,14 +31,15 @@ def three_most_popular_articles():
     print()
 
     return
+
 
 def most_popular_authors():
-    """Queries and returns top three most viewed articles in the news database."""
+    """Queries and displays the Authors with the most views."""
     conn = psycopg2.connect(database=DBNAME)
 
     cur = conn.cursor()
 
-    query = 'VIEW_top_three_articles'
+    query = 'VIEW_most_popular_authors'
 
     cur.execute(query)
 
@@ -46,21 +48,52 @@ def most_popular_authors():
     cur.close()
 
     conn.close()
-    
+
     print()
-    print('Three most popular articles of all time')
+    print('Three most popular authors')
     print('=======================================')
 
     for result in results:
-        print('"{title}" - {count} views'
-              .format(title=result[0], count=result[1]))
+        print('"{author}" - {count} views'
+              .format(author=result[0], count=result[1]))
     print()
 
     return
+
+
+def days_with_high_errors():
+    """Queries and displays the days when errors were above 1%."""
+    conn = psycopg2.connect(database=DBNAME)
+
+    cur = conn.cursor()
+
+    query = 'VIEW_days_with_over_one_percent_errors'
+
+    cur.execute(query)
+
+    result = cursor.fetchall()
+
+    cur.close()
+
+    conn.close()
+
+    print()
+    print('Days with over 1% errors')
+    print('=======================================')
+
+    for result in results:
+        print('"{day}" - {error_rate} errors'
+              .format(day=result[0], error_rate=result[1]))
+    print()
+
+    return
+
+
 def main():
     three_most_popular_articles()
+    most_popular_authors()
+    days_with_high_errors()
+
 
 if __name__ == '__main__':
     main()
-
-    
